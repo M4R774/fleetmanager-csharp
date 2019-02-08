fcef6a1914c45ffeaab05799339e53d9660760e3
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2019-02-08 17:19:36 +0200

    Add variables and methods for car entity

--- Eatech.FleetManager.ApplicationCore/Entities/Car.cs
+++ Eatech.FleetManager.ApplicationCore/Entities/Car.cs
@@ -5,7 +5,12 @@ namespace Eatech.FleetManager.ApplicationCore.Entities
     public class Car
     {
         public Guid Id { get; set; }
-
+        public String Brand { get; set; }
+        public String Model { get; set; }
+        public String RegistrationNumber { get; set; }
         public int ModelYear { get; set; }
+        public DateTime InspectionDate { get; set; }
+        public int EngineDisplacement { get; set; }
+        public int EnginePower { get; set; }
     }
 }
